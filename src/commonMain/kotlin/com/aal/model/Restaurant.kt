2c946ff51416e7624742ef5eff0ef266c0544252
mitchdrew <mitchdrew@goatse.cx>
2021-04-13 17:55:48 -0700

    master - mdrew: price to int

--- src/commonMain/kotlin/com/aal/model/Restaurant.kt
+++ src/commonMain/kotlin/com/aal/model/Restaurant.kt
@@ -1,4 +1,4 @@
 package com.aal.model
 
-data class Restaurant(val iconRef:String, val name:String, val formattedAddress:String, val price:String, val score:Double, val numReviews:Int, val lat:Double, val lng:Double)
+data class Restaurant(val iconRef:String, val name:String, val formattedAddress:String, val price:Int, val score:Double, val numReviews:Int, val lat:Double, val lng:Double)
 
